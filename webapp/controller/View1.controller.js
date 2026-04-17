1aee87e87636f83a36024e1651c5a7b12fb0f9e3
Rickccastro <ricardoparmezancastro.dev@gmail.com>
2026-04-17 16:23:25 -0300

    feat: i18n files

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -6,10 +6,21 @@ function (Controller) {
 
     return Controller.extend("fiorifreestyle.controller.View1", {
         onInit: function () {
-
+        var oView  = this.getView()
+        var oModel = new sap.ui.model.json.JSONModel()
+        
+        oModel.setData({ "usuario": {"nome": "Ricardo"} })
+        oView.setModel(oModel)
         },
+
         onPress: function (){
             alert("Hello")
+        },
+        
+        onShowMessage: function(){
+            var sTitle = this.getView().getModel("i18n").getResourceBundle().getText("customerName")
+            alert(sTitle)
+
         }
     });
 });
